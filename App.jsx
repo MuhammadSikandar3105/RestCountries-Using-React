2cffa09ab81912215f16050bba12e01a9327c558
MalkuSR <m.s.malik3105@gmail.com>
2024-12-15 17:34:09 +0500

    improve

--- App.jsx
+++ App.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-import './index.css';
-import Maincontent from './components/Maincontent';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SpecificCountry from './components/SpecificCountry';
+import React from "react";
+import Header from "./components/Header";
+import "./index.css";
+import Maincontent from "./components/Maincontent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SpecificCountry from "./components/SpecificCountry";
 
 const App = () => {
-    
-    return (
-            <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true, }}>
-                <Header />
-                <Routes >
-                    <Route path='/' element={<Maincontent  />} />
-                    <Route path='/:country' element={<SpecificCountry />} />
-                </Routes>
-            </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
+      <Header />
+      <Routes>
+        <Route path="/" element={<Maincontent />} />
+        <Route path="/:country" element={<SpecificCountry />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
